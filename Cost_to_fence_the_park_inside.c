5e65e9615f0dc25428c33644e3bf09e4875e7523
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2022-02-18 14:35:36 +0530

    #include<stdio.h> int main() {     int l,b,w,c,f_area;     scanf("%d%d%d%d",&l,&b,&w,&c);     if(l<=2*w||b<=2*w)     printf("Impossible");     else     {         f_area=(l*b)-((l-2*w)*(b-2*w));         printf("%d",f_area*c);     } }

---
+++ Cost_to_fence_the_park_inside.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,w,c,f_area;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    if(l<=2*w||b<=2*w)
+    printf("Impossible");
+    else
+    {
+        f_area=(l*b)-((l-2*w)*(b-2*w));
+        printf("%d",f_area*c);
+    }
+}
